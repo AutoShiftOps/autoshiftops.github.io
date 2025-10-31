9866fb1020cacfdd76a6ef99e4ae8163375dd4b8
devops-den <devops-den@users.noreply.github.com>
2025-10-31 12:16:11 +0000

    deploy: 7aed94c60dce30ed8288220beb7d0c990812a222

--- mkdocs_bakcup/javascripts/scroll-top.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("scroll", function() {
-  const btn = document.getElementById("scrollTopBtn");
-  btn.style.display = window.scrollY > 300 ? "block" : "none";
-});
-
-function scrollToTop() {
-  window.scrollTo({ top: 0, behavior: "smooth" });
-}
